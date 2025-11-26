af3b4977532e452b76b4669f9041fe71ae1bef59
seanf870 <44120999+MyNameIsTakenOMG@users.noreply.github.com>
2025-11-26 14:59:02 -0500

    Modify backend configuration and add DynamoDB table
    Commented out auth and data resources in the backend configuration and added a new DynamoDB table.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,23 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import {
+  AttributeType,
+  GlobalSecondaryIndexPropsV2,
+  ProjectionType,
+  StreamViewType,
+  TableV2,
+} from 'aws-cdk-lib/aws-dynamodb';
 
 defineBackend({
-  auth,
-  data,
+  // auth,
+  // data,
+});
+const dbStack = backend.createStack('db-stack');
+const myTable = new TableV2(stack, 'dentabook2-db-table', {
+  partitionKey: {
+    name: 'pk',
+    type: AttributeType.STRING,
+  },
+  removalPolicy: RemovalPolicy.DESTROY,
 });
